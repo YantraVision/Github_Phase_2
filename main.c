d508776ec42055d38219a688a9ea77f55506c2db
Hariharan73958 <hariharan.s@yantravision.com>
2024-12-17 15:38:49 +0530

    Division by Hariharan S

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 void add(int number1,int number2);
 void subraction(int number1,int number2);
 void multiply(int number1,int number2); //program to multiply two number
+void division(int number1,int number2); //program to divide two number
 int main (){
 int number1 = 20; //sample input value1
 int number2 = 2;  //sample input value2
 add(number1,number2);
 subraction(number1,number2);   
 multiply(number1,number2);
+division(number1,number2);
 }
 void add(int number1,int number2){
 int add_result;
@@ -26,4 +28,10 @@ void multiply(int number1,int number2){
 int result=0;
 result=number1*number2;
 printf("Multiplication of Two numbers is %d",result);
+}
+
+void division(int number1,int number2){
+int result=0;
+result=number1/number2;
+printf("Division of Two numbers is %d",result);
 }
