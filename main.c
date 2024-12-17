04102822588329229271df4af30d23a8ac17a40c
Hariharan73958 <hariharan.s@yantravision.com>
2024-12-17 15:02:32 +0530

    add function by Hariharan

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 //main is created by Kishore.V
 
 #include<stdio.h> //headerfile
-
+void add(int num1,int num2);
 int main ()
 {
    int num1 = 20; //sample input value1
    int num2 = 2;  //sample input value2
+}
+void add(int num1,int num2){
+   int add_result;
+   add_result=num1+num2;
+   printf("Addition of Two number is %d",add_result);
 }
