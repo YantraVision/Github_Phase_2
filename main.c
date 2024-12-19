e37b3a649fc670854e1caad9f37e8b66909c9e64
Kishore02721 <kishore.k@yantravision.com>
2024-12-19 12:24:24 +0530

    Changes done in multiplication

--- main.c
+++ main.c
@@ -3,14 +3,15 @@
 #include<stdio.h> //headerfile
 void add(int number1,int number2);
 void subraction(int number1,int number2);
-void multiply(int number1,int number2); //program to multiply two number
+void multiply(int number1,int number2,int number3); //program to multiply two number
 void division(int number1,int number2); //program to divide two number
 int main (){
 int number1 = 20; //sample input value1
 int number2 = 2;  //sample input value2
+int number3 = 3; //sample input value3
 add(number1,number2);
 subraction(number1,number2);   
-multiply(number1,number2);
+multiply(number1,number2,number3);
 division(number1,number2);
 }
 void add(int number1,int number2){
@@ -24,7 +25,7 @@ result=number1-number2;
 printf("Subraction of Two numbers is %d",result);
 }
 
-void multiply(int number1,int number2){
+void multiply(int number1,int number2, int number3){
 int result=0;
 result=number1*number2;
 printf("Multiplication of Two numbers is %d",result);
